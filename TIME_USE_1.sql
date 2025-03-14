eb1dd9156864bda252bbb633334250cb62e584ba
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-03-14 09:55:57 +0200

    22

--- TIME_USE_1.sql
+++ TIME_USE_1.sql
@@ -1,3 +1,9 @@
+        SELECT     
+                   FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   R.DENUMIRE
+                   
+              FROM(    
       SELECT FC.CUIIO,
                    FC.CUIIO_VERS,
                    FC.FORM,
@@ -10,4 +16,7 @@
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (102) AND FC.STATUT <> '3'
+             WHERE FC.FORM IN (102) AND FC.STATUT <> '3' ) FC 
+             
+                            INNER JOIN CIS2.RENIM R ON R.CUIIO = FC.CUIIO 
+                                    AND R.CUIIO_VERS = FC.CUIIO_VERS
